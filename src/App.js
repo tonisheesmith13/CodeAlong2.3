03ed6cd7dd5638a3f5ae3f68765281f6301bf432
Toni Smith <tonisheesmith@hotmail.com>
2022-12-21 05:41:59 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="app">
-      <h1 className="title">Code Nation Creamery Yay</h1>
+      <h1 className="title">Code Nation Creamery Yay new name</h1>
       <div className="menu">
         <h2>Menu Choices:</h2>
       </div>
